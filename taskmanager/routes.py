953fc312e639ae0c516ae6e9ea447ec40641d83c
Vikmah <viktoria.mahrberg@gmail.com>
2022-03-28 18:18:28 +0000

    Add tasks function to display

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -5,7 +5,8 @@ from taskmanager.models import Category, Task
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
@@ -52,7 +53,7 @@ def add_task():
             is_urgent=bool(True if request.form.get("is_urgent") else False),
             due_date=request.form.get("due_date"),
             category_id=request.form.get("category_id")
-    ) 
+        )
         db.session.add(task)
         db.session.commit()
         return redirect(url_for("home"))
